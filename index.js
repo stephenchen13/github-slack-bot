3e1f3f2793e678dd647edcb64bec92f57b6dfe9d
Stephen Chen <stephenchen13@gmail.com>
2016-03-11 14:46:00 -0800

    Add body parser

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -14,7 +16,6 @@ app.get('/', function(request, response) {
 });
 
 app.post('/github_webhook', function(request, response) {
-  console.log(request);
   console.log(request.body);
   response.sendStatus(200);
 });
